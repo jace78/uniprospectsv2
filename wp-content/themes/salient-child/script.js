9e00741a89c0485740a0d95295039ee20c5c2c65
Jean-Sébastien Godard <jean-sebastien.godard@polyconseil.fr>
2024-09-16 20:04:47 +0200

    js logs

--- wp-content/themes/salient-child/script.js
+++ wp-content/themes/salient-child/script.js
@@ -1,6 +1,8 @@
 /* Hide form after submit */
 document.addEventListener( 'wpcf7mailsent', function( event ) {
+  console.log('hello wpcf7mailsent');
   document.querySelectorAll("form.wpcf7-form > :not(.wpcf7-response-output)").forEach(el => {
+    console.log(el);
     el.style.display = 'none';
   });
 }, false );

29ba9a8270cccc119f0b4618ca3da59c3a04eb26
Jean-Sébastien Godard <jean-sebastien.godard@polyconseil.fr>
2024-09-30 20:06:49 +0200

    update code source from v2.uniprospects.com

---
+++ wp-content/plugins/simple-history/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-date', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-url'), 'version' => '644a543d776bc44abba4');
